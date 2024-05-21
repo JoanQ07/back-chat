3ba7740288d1e4405a0c1ab2b4bdd04a7af51b0b
Joan Quintero <jhoanquintero07@hotmail.com>
2024-05-21 12:27:45 -0500

    [Feat] Se integran rutas

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
+import routesNotification from "./modules/notifications/route";
 import routesMessage from "./modules/messages/route";
-import routesUser from "./modules/user/route";
+import routesFriend from "./modules/friends/route";
 import routesChat from "./modules/chats/route";
+import routesUser from "./modules/user/route";
 import { Router } from "express";
 
 const router = Router();
 
 router.use("/api", ((req, res) => res.send("API-REST ON 🟢")));
+router.use("/notification", routesNotification);
 router.use("/message", routesMessage);
+router.use("/friend", routesFriend);
 router.use("/user", routesUser);
 router.use("/chat", routesChat);
 
